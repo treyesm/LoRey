8c7e243efc0d30cbdabc18f18fcb4b3e436c28b2
treyesm <tania.innox@gmail.com>
2015-10-20 00:12:47 -0500

    Capitulo 13
    Test con javascript

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+    
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -47,18 +50,16 @@ class ItemValidationTest(FunctionalTest):
         self.assertFalse(error.is_displayed())
 
     def test_error_messages_are_cleared_on_input(self):
-        # Edith starts a new list in a way that causes a validation error:
+         # Edith starts a new list in a way that causes a validation error:
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
         error = self.get_error_element()
-        self.assertFalse(error.is_displayed())
+        self.assertTrue(error.is_displayed())
 
         # She starts typing in the input box to clear the error
         self.get_item_input_box().send_keys('a')
+
         # She is pleased to see that the error message disappears
         error = self.get_error_element()
         self.assertFalse(error.is_displayed())
 
-
-    def get_error_element(self):
-        return self.browser.find_element_by_css_selector('.has-error')
