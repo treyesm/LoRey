0947cbd6249ea7675491279c3c372184f95a95b9
treyesm <tania.innox@gmail.com>
2015-10-19 23:38:25 -0500

    Capitulo 13
    Primera prueba FT

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -43,5 +43,22 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You've already got this in your list")
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a new list in a way that causes a validation error:
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+        # She is pleased to see that the error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
